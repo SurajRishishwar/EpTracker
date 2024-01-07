7252c797ddb324eadc1734e0781b865883157e8a
Suraj Sharma <rishishwarsuraj@gmail.com>
2024-01-07 15:26:28 +0530

    Creating Repo

---
+++ index.js
@@ -0,0 +1,38 @@
+const express= require("express");
+const port=8000||process.env.port;
+const app=express();
+const mysql = require("mysql2");
+
+const mysqlconnection=mysql.createPool({
+    host : "sql.freedb.tech",   
+    port : 3306,
+    database : "freedb_EpTracker",
+    user : "freedb_access-suraj-to",
+    password : "7bSAC*@xbpTuf*U",
+    connectionLimit: 20,
+    multipleStatements:true
+});
+
+mysqlconnection.getConnection((err)=>{
+    if(err){
+        console.log('Not Connected',err);
+    }
+    else{
+        console.log('Connection Established');
+    }
+});
+
+app.get('/expense_of_0310_testing',(req,res)=>{
+    let sql="select * from expense_of_user";
+    let query=mysqlconnection.query(sql,(err,results)=>{
+        if(err){
+            console.log(err);
+        }else{
+            res.status(200).send(results);
+        }
+    })
+})
+
+app.listen(port,()=>{
+    console.log("server is up at : "+port);
+});
